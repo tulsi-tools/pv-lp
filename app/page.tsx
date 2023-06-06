df115c5972fe49379176bffd9ee37b8b06beadf2
Neel <neel738@hotmail.com>
2023-06-06 16:14:39 +0200

    added flywheel

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import SquigglyLines from "../components/SquigglyLines";
-
 export default function HomePage() {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
@@ -39,27 +38,15 @@ export default function HomePage() {
         </Link>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
-            <div className="flex sm:space-x-8 sm:flex-row flex-col">
-              <div>
-                <h3 className="mb-1 font-medium text-lg">Original Room</h3>
-                <Image
-                  alt="Original photo of a room with roomGPT.io"
-                  src="/original-pic.jpg"
-                  className="w-full object-cover h-96 rounded-2xl"
-                  width={400}
-                  height={400}
-                />
-              </div>
-              <div className="sm:mt-0 mt-8">
-                <h3 className="mb-1 font-medium text-lg">Generated Room</h3>
-                <Image
-                  alt="Generated photo of a room with roomGPT.io"
-                  width={400}
-                  height={400}
-                  src="/generated-pic-2.jpg"
-                  className="w-full object-cover h-96 rounded-2xl sm:mt-0 mt-2"
-                />
-              </div>
+            <div className="flex sm:space-x-8 sm:flex-row flex-col bg-white rounded-2xl p-10">
+              <Image
+                alt="Generated photo of a room with roomGPT.io"
+                width={400}
+                height={400}
+                src="/flywheel.svg"
+                className="w-full object-cover h-96 rounded-2xl sm:mt-0 mt-2"
+              />
+
             </div>
           </div>
         </div>
